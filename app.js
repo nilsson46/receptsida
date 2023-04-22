c67499a528509d17d616be1fe681469be2316a84
nilsson46 <nilssonsimon46@gmail.com>
2023-04-22 16:40:26 +0200

    Started with some css. And fixed some javascript

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-/*document.addEventListener("DOMContentLoaded", function() {
-    const portionSizeInput = document.querySelector(".portion-size:not(.result)");
-    const numberOfPortionsInput = document.querySelector(".portions");
-    const resultElement = document.querySelector(".result");
-    const portionSizeDisplay = document.querySelector(".portion-size-display");
-
-    function calculateAmount() {
-        const sizeOfProduct = parseInt(portionSizeInput.value);
-        const amountOfPortions = parseInt(numberOfPortionsInput.value);
-        console.log('sizeOfProduct:', sizeOfProduct);
-        console.log('amountOfPortions:', amountOfPortions);
-        const result = sizeOfProduct * amountOfPortions;
-        resultElement.textContent = result;
-        portionSizeDisplay.textContent = sizeOfProduct;
-    }
-
-    portionSizeInput.addEventListener("input", calculateAmount);
-    numberOfPortionsInput.addEventListener("input", calculateAmount);
-}); 
-*/
 const portionSize = document.querySelector(".portions");
 console.log(portionSize.value);
 
 const ingredientsSize = document.querySelector(".ingredients-size");
 console.log(ingredientsSize.value); 
+const amountEl = document.querySelector("#amount");
 
 function calculateAmount(){
     totalIngredients = portionSize.value*ingredientsSize.value;
@@ -31,9 +12,10 @@ function calculateAmount(){
 
 function updateAmount() {
     const amount = calculateAmount();
-    console.log(amount);
-    // Update the UI to show the new amount, e.g.:
-    // document.querySelector(".amount").textContent = amount;
-}
+    amountEl.textContent = amount;
+  }
+  
 
-portionSize.addEventListener("change", updateAmount);
+updateAmount();
+  
+portionSize.addEventListener("input", updateAmount);
