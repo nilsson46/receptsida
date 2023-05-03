9bc88c10808c4b803f081a8db3f2291f0acde466
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2023-05-03 16:06:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,81 @@
-const portionsInput = document.querySelector(".portions");
-const ingredientSizes = document.querySelectorAll(".ingredients-size");
-const amountSpans = document.querySelectorAll(".amount");
 const commentField = document.getElementById("comment");
 const checkboxes = document.querySelectorAll(".checkbox");
 const steps = document.querySelectorAll(".steps");
 
-function updateIngredientAmounts() {
-  const portions = portionsInput.value;
-  let originalValue, newValue = undefined;
-  ingredientSizes.forEach((sizeInput, index) => {
-    originalValue = sizeInput.getAttribute("value");
-    newValue = originalValue * portions;
-    amountSpans[index].textContent = `${newValue}`;
-  });
-}
+const ingredientElements = [];
 
-updateIngredientAmounts();
-
-portionsInput.addEventListener("change", updateIngredientAmounts);
-
-
-
-function checkbox() {
-    checkboxes.forEach(function(checkbox, index) {
-        checkbox.addEventListener("change", function() {
-            if (checkbox.checked) {
-                steps[index].style.color = "grey";
-            } else {
-                steps[index].style.color = "initial";
-            }
-        });
-    });
-}
-
-checkbox();
-
-const post = document.getElementById("post");
-post.addEventListener("click", function(){
-    const commentValue = document.getElementById("comment").value;
-    if(commentValue == ""){
-        return;
+const comment = {
+    field: document.querySelector('#comment'),
+    list: document.querySelector('#unordered'),
+    create: (content) => {
+        const li = document.createElement('li');
+        li.className = 'comment';
+        li.textContent = content;
+        comment.list.append(li);
     }
-    const li = document.createElement("li");
-    li.className = "comment"
-    const text = document.createTextNode(commentValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
-    document.getElementById("comment").value ="";
+}
+
+class IngredientElement {
+    constructor(el) {
+        this.amountField = el.querySelector('.ingredient-size');
+        this.portionSpan = el.querySelector('.amount');
+    }
+
+    updateAmount(newPortions) {
+        this.portionSpan.textContent = this.amountField.value * newPortions;
+    }
+}
+
+const handleNewPortions = event => {
+    const newPortion = event.target.value;
+    ingredientElements.forEach(el => el.updateAmount(newPortion));
+};
+
+const handleStepCheckboxChange = (checkbox, stepItem) => {
+    if (checkbox.checked) {
+        stepItem.style.color = "grey";
+    } else {
+        stepItem.style.color = "initial";
+    }
+}
+
+const handleNewComment = () => {
+    const content = comment.field.value;
+    if(content == '') return false; // guardian clause (returnerar false)
+    
+    comment.create(content);
+    comment.field.value = '';
+
     let totalRatingDiv = document.querySelector(".total-rating");
     const averageRating = totalStarValue / totalStarsClicked;
     totalRatingDiv.textContent = "Snittbetyg: " + averageRating.toFixed(1);
-});
+}
+
+const attachCheckboxListener = (stepItem) => {
+    const checkbox = stepItem.querySelector('.checkbox');
+    checkbox.addEventListener('change', () => handleStepCheckboxChange(checkbox, stepItem));
+}
+
+
+document
+    .querySelectorAll('.ingredient')
+    .forEach(el => ingredientElements.push(new IngredientElement(el)));
+
+document
+    .querySelector('.portions')
+    .addEventListener('change', handleNewPortions);
+
+document
+    .querySelectorAll('.steps')
+    .forEach(stepItem => attachCheckboxListener(stepItem));
+
+document
+    .querySelector('#post')
+    .addEventListener('click', handleNewComment);
+
+
+
+
 
 let starValue = null;
 
